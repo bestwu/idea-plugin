08fbeaec0443828ed07b6ca12a52e67272660973
Peter Wu <piterwu@outlook.com>
2017-09-13 10:59:25 +0800

    use cn.bestwu.idea plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,16 @@
-
 import org.gradle.kotlin.dsl.*
 
 plugins {
     kotlin("jvm") version "1.1.4-3"
     id("cn.bestwu.plugin-publish").version("0.0.12")
+    id("cn.bestwu.idea") version "0.0.1"
 }
 
 group = "cn.bestwu.gradle"
 version = "0.0.1"
 
-
-
 kotlin {
-    copyClassesToJavaOutput=true
+    copyClassesToJavaOutput = true
 }
 
 repositories {
