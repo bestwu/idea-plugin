3e14466e99e130c1ffb2aeaf1087d7f331c22a70
Peter Wu <piterwu@outlook.com>
2017-09-16 22:59:36 +0800

    use 0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.*
 plugins {
     kotlin("jvm") version "1.1.4-3"
     id("cn.bestwu.plugin-publish").version("0.0.12")
-    id("cn.bestwu.idea") version "0.0.2"
+    id("cn.bestwu.idea") version "0.0.4"
 }
 
 group = "cn.bestwu.gradle"
